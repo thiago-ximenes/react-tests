86f86303716f85125d0c1484c48d7aea45241365
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-12-05 20:06:03 -0300

    🚨 lint: erase line with userEvent import.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,4 @@
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from '../helper/renderWithRouter';
