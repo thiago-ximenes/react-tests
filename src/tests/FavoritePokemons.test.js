ec6ed2d42b1bc3bb918f6b312ce7c33ea3ea4de1
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2021-12-06 17:18:00 -0300

    ✅ pass test: requiriment 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from '../helper/renderWithRouter';
+
+describe('Check Favorite Pokemons', () => {
+  it('Check if a msg render as there is not a favorite pokemon yet', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noPokemonMsg = screen.getByText('No favorite pokemon found');
+    expect(noPokemonMsg).toBeInTheDocument();
+  });
+  it('', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/');
+    const moreDetailsBtn = screen.getByText('More details');
+    userEvent.click(moreDetailsBtn);
+    const favoriteButton = screen.getByText('Pokémon favoritado?');
+    userEvent.click(favoriteButton);
+    const favoriteLink = screen.getByText('Favorite Pokémons');
+    userEvent.click(favoriteLink);
+    const favoritesPokemons = screen.getAllByText('More details');
+    expect(favoritesPokemons).toHaveLength(1);
+  });
+});
