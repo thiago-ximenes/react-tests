6cea7c386a528a5f90f35435c0b7f0f4962f1197
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2021-12-06 18:09:34 -0300

    ✅ pass test: requiriment 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../helper/renderWithRouter';
+
+describe('Check the component NotFound', () => {
+  test('', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundTitle = screen.getByRole(
+      'heading', { name: 'Page requested not found Crying emoji', level: 2 },
+    );
+    expect(notFoundTitle).toBeInTheDocument();
+
+    const imgNotFound = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(imgNotFound.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
